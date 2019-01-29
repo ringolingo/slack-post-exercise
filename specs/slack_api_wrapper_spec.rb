9d8f2e2ad0160fcaa5646f3beffdc973511bed40
CheezItMan <chris@adadevelopersacademy.org>
2019-01-29 11:16:46 -0800

    initial scaffold

--- specs/slack_api_wrapper_spec.rb
+++ specs/slack_api_wrapper_spec.rb
@@ -2,20 +2,5 @@
 require_relative 'spec_helper'
 
 describe SlackApiWrapper do
-  it "can send a valid tweet" do
-    VCR.use_cassette("posts") do
-      response = SlackApiWrapper.send_msg('Hey I can post messages!', ENV['CHANNEL_NAME'])
-      expect(response).must_equal true
-    end
-  end
 
-  it "will raise an error when given an invalid channel" do
-    VCR.use_cassette("posts") do
-      exception = expect {
-        SlackApiWrapper.send_msg("Hey I can post messages!", "invalid-channel")
-      }.must_raise StandardError
-
-      expect(exception.message).must_equal "channel_not_found"
-    end
-  end
 end
