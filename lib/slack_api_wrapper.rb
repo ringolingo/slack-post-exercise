54f959c691c24363bda26d594ab5675bc80beb52
CheezItMan <chris@adadevelopersacademy.org>
2019-01-29 11:16:46 -0800

    initial scaffold

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -1,6 +1,5 @@
 # slack_api_wrapper.rb
 require 'httparty'
-require 'awesome_print'
 
 
 class SlackApiWrapper
@@ -8,24 +7,5 @@ class SlackApiWrapper
   API_KEY = ENV['SLACK_TOKEN']
 
   # Code goes here
-  def self.send_msg(message, channel)
-
-    response = HTTParty.post(
-      "#{BASE_URL}/chat.postMessage",
-      body:  {
-        token: API_KEY,
-        text: message,
-        channel: channel
-      },
-      headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
-    )
-
-    response_body = JSON.parse(response.body)
-
-    unless response.success? && response_body["ok"]
-      raise StandardError, response_body["error"] 
-    end
-
-    return true
-  end
+  
 end
