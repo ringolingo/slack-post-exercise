1ddbe8a9f696290bac0fe80055afbd772172eb82
CheezItMan <chris@adadevelopersacademy.org>
2019-01-31 13:54:11 -0800

    class --> module

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -1,11 +1,10 @@
 # slack_api_wrapper.rb
-require 'httparty'
+require "httparty"
 
-
-class SlackApiWrapper
-  BASE_URL = 'https://slack.com/api/'
-  API_KEY = ENV['SLACK_TOKEN']
+module SlackApi
+  BASE_URL = "https://slack.com/api/"
+  API_KEY = ENV["SLACK_TOKEN"]
 
   # Code goes here
-  
+
 end
