1b34194234a73aaf3d8d4d3d1ac0370d942bcbb0
CheezItMan <chris@adadevelopersacademy.org>
2019-03-20 13:33:07 -0700

    send_msg completed

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -5,6 +5,23 @@ module SlackApi
   BASE_URL = "https://slack.com/api/"
   API_KEY = ENV["SLACK_TOKEN"]
 
-  # Code goes here
+  class SlackError < StandardError; end
 
+  # Code goes here
+  def self.send_msg(text, channel)
+    response = HTTParty.post(
+      "#{BASE_URL}chat.postMessage",
+      headers: {"Content-Type" => "application/x-www-form-urlencoded"},
+      body: {
+        token: API_KEY,
+        channel: channel,
+        text: text,
+      },
+    )
+    if response["ok"]
+      return true
+    else
+      raise SlackApi::SlackError, "Error when posting #{text} to #{channel}, error: #{response["error"]}"
+    end
+  end
 end
--- specs/slack_api_wrapper_spec.rb
+++ specs/slack_api_wrapper_spec.rb
@@ -1,6 +1,29 @@
 # slack_api_wrapper_spac.rb
-require_relative 'spec_helper'
+require_relative "spec_helper"
 
-describe SlackApiWrapper do
+describe SlackApi do
+  describe "send_msg" do
+    it "can send a valid message" do
+      VCR.use_cassette("slack_message") do
+        return_value = SlackApi.send_msg("Test message",
+                                         "ports-api-testing")
 
+        expect(return_value).must_equal true
+      end
+    end
+
+    it "generates an error if given an invalid channel" do
+      VCR.use_cassette("slack_message") do
+        expect {
+          SlackApi.send_msg("Test message",
+                            "bogus")
+        }.must_raise SlackApi::SlackError
+      end
+    end
+    it "will generate an error if given an invalid key" do
+    end
+
+    it "will raise an error if given an empty message" do
+    end
+  end
 end
